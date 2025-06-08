69cac9cd43d606b466994c62a2268bdb407995df
Charles Zhang <charleszhangmb@gmail.com>
2025-06-07 17:17:39 -0700

    Prettier

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,53 +1,61 @@
-"use client";
+'use client';
 
-import MobileMenu from "@/components/MobileMenu/MobileMenu";
-import { isActiveRoute } from "@/utils/helpers";
-import { LinkItem } from "@/utils/interfaces";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React, { useState } from "react";
-import styles from "./Navbar.module.scss";
+import MobileMenu from '@/components/MobileMenu/MobileMenu';
+import { isActiveRoute } from '@/utils/helpers';
+import { LinkItem } from '@/utils/interfaces';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React, { useState } from 'react';
+import styles from './Navbar.module.scss';
 
 const Navbar: React.FC = () => {
-	// Constants
-	const LINK_ITEMS: LinkItem[] = [
-		{ display: "Home", href: "/" },
-		{ display: "Projects", href: "/projects" },
-	];
+    // Constants
+    const LINK_ITEMS: LinkItem[] = [
+        { display: 'Home', href: '/' },
+        { display: 'Projects', href: '/projects' },
+    ];
 
-	// Hooks
-	const pathname = usePathname();
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+    // Hooks
+    const pathname = usePathname();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	// Helpers
-	const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-	const closeMenu = () => setIsMenuOpen(false);
+    // Helpers
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+    const closeMenu = () => setIsMenuOpen(false);
 
-	// JSX
-	return (
-		<nav className={styles.navbar}>
-			<div className={styles.container}>
-				{/* Logo */}
-				<Link href="/" onClick={closeMenu}>
-					<span className={styles.logo}>CZHANGY.IO</span>
-				</Link>
+    // JSX
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.container}>
+                {/* Logo */}
+                <Link href="/" onClick={closeMenu}>
+                    <span className={styles.logo}>CZHANGY.IO</span>
+                </Link>
 
-				{/* Desktop Navigation */}
-				<ul className={styles.navLinks}>
-					{LINK_ITEMS.map((item: LinkItem) => (
-						<li key={item.href}>
-							<Link href={item.href} className={`${styles.navLink} ${isActiveRoute(pathname, item.href) ? styles.active : ""}`}>
-								{item.display}
-							</Link>
-						</li>
-					))}
-				</ul>
+                {/* Desktop Navigation */}
+                <ul className={styles.navLinks}>
+                    {LINK_ITEMS.map((item: LinkItem) => (
+                        <li key={item.href}>
+                            <Link
+                                href={item.href}
+                                className={`${styles.navLink} ${isActiveRoute(pathname, item.href) ? styles.active : ''}`}
+                            >
+                                {item.display}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
 
-				{/* Mobile Navigation */}
-				<MobileMenu open={isMenuOpen} items={LINK_ITEMS} onToggle={toggleMenu} onClose={closeMenu} />
-			</div>
-		</nav>
-	);
+                {/* Mobile Navigation */}
+                <MobileMenu
+                    open={isMenuOpen}
+                    items={LINK_ITEMS}
+                    onToggle={toggleMenu}
+                    onClose={closeMenu}
+                />
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
